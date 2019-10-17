fd06ecb795324acb00bb58b1478f3a9b22c9f85d
Ryan Groesch <ryan.groesch@gmail.com>
2019-10-17 14:07:00 -0700

    Day 18/19

--- src/pkg30daysofcode/Day18.java
+++ src/pkg30daysofcode/Day18.java
@@ -1,2 +1,55 @@
+import java.io.*;
+import java.util.*;
+
 public class Day18 {
+
+    Queue<Character> qq = new LinkedList<>();
+    Stack<Character> ss = new Stack<>();
+
+    void pushCharacter(char ch) {
+        ss.push(ch);
+    }
+
+    char popCharacter() {
+        return ss.pop();
+    }
+
+    void enqueueCharacter(char ch) {
+        qq.add(ch);
+    }
+
+    char dequeueCharacter() {
+        return qq.remove();
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        scan.close();
+
+        // Convert input String to an array of characters:
+        char[] s = input.toCharArray();
+
+        // Create a Solution object:
+        Day18 p = new Day18();
+
+        // Enqueue/Push all chars to their respective data structures:
+        for (char c : s) {
+            p.pushCharacter(c);
+            p.enqueueCharacter(c);
+        }
+
+        // Pop/Dequeue the chars at the head of both data structures and compare them:
+        boolean isPalindrome = true;
+        for (int i = 0; i < s.length/2; i++) {
+            if (p.popCharacter() != p.dequeueCharacter()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        //Finally, print whether string s is palindrome or not.
+        System.out.println( "The word, " + input + ", is "
+                + ( (!isPalindrome) ? "not a palindrome." : "a palindrome." ) );
+    }
 }
